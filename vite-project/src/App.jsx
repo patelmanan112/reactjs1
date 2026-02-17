553fe91c5436c1ad5ea2f91198e8a45d2823c382
patelmanan112 <manan.patel.cg@gmail.com>
2026-02-17 11:38:58 +0530

    adding task of useRefrence

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,17 +1,37 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
+import { useReducer } from 'react'
 import viteLogo from '/vite.svg'
 import './App.css'
 import SignUp from './components/signUp'
+const intialState = {
+  count :Number(localStorage.getItem("count")),
+  name :  ""
+}
+
+function reducer(state , action){
+  switch(action.type){
+    case "Inc" : return {...state , count : state.count+1}
+    case "Dec" : return {...state , count : state.count -1}
+    case "name" : return {...state , name : "Manan"}
+  }
+}
 function App() {
-  const [count, setCount] = useState(0)
-  const getdata = localStorage.getItem("userData")
-  console.log(getdata);
-  
+  const[state , dispatch] = useReducer(reducer,intialState )
+  useEffect(()=>{
+    localStorage.setItem("count" , state.count);
+    console.log(localStorage.getItem("count"));
+    
+  }, [state.count])
   return (
     <>
-     <SignUp/>
-     <p>{getdata.name}</p>
+      <SignUp />
+    
+      <h1>Count : {state.count}</h1>
+      <h1>Name : {state.name}</h1>
+      <button onClick={() => {dispatch({type : 'Inc'})}}>Increse</button>
+      <button onClick={() => { dispatch({type : 'Dec'})}}>Decrease</button>
+      <button onClick={() => { dispatch({type : 'name'})}}>Reset</button>
     </>
   )
 }
